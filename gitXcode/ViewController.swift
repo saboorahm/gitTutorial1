c7a86c02d7bb97e4c38d9e8026738eb820b24d3e
Saboor Ahmad <saboorahmadsatti2@gmail.com>
2021-03-06 15:33:04 +0500

    Initial Commit

---
+++ gitXcode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gitXcode
+//
+//  Created by Apple on 06/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
